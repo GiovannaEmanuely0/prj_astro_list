b1ca450fd4dd9967a14a1d7cdc33dd8c2eb53e99
Giovanna Emanuely <giovannaEmma16@gmail.com>
2026-06-02 08:36:03 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,17 +7,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Rotas do usuário
+// Rotas do usuário get - post - put
 Route::get('/usuario','App\Http\Controllers\userController@indexApi');
 Route::post('/usuario','App\Http\Controllers\CadastroController@storeApi');
 Route::put('/usuario/{id}','App\Http\Controllers\userController@updateApi');
+Route::delete('/usuario/{id}','App\Http\Controllers\userController@destroyApi');
+Route::get('/usuario/{id}','App\Http\Controllers\userController@countUsuario');
 
 // Rotas da tarefa
 Route::get('/tarefa','App\Http\Controllers\tarefaController@indexApi');
 Route::post('/tarefa','App\Http\Controllers\tarefaController@storeApi');
 Route::put('/tarefa/{id}','App\Http\Controllers\tarefaController@updateApi');
+Route::delete('/tarefa/{id}','App\Http\Controllers\tarefaController@destroyApi');
+Route::get('/tarefa/{id}','App\Http\Controllers\tarefaController@countTarefa');
 
-// Rotas do Adm
+//Rotas do Adm
 Route::get('/admin','App\Http\Controllers\adminController@indexApi');
 Route::post('/admin','App\Http\Controllers\adminController@storeApi');
 Route::put('/admin/{id}','App\Http\Controllers\adminController@putApi');
