93fdc905968cb8f75bf0403871b11b2ebbe1ef12
Giovanna Emanuely <giovannaEmma16@gmail.com>
2026-08-31 21:18:05 -0300

    Rename TarefaSeeder to UsuarioSeeder and update data

--- database/seeders/TarefaSeeder.php
+++ database/seeders/TarefaSeeder.php
@@ -4,10 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Support\Facades\DB;
 
-class tarefaSeeder extends Seeder
+class usuarioSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -16,27 +17,17 @@ class tarefaSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('tarefa')->insert([
-            
-            ['titulo'=> 'Estudar POO', 'descricao'=>'Aprender herança, encapsulamento, classes e método...', 'dataInicio'=>'2026-03-23', 'dataTermino'=>'2026-03-30', 'status'=>'Pendente', 'prioridade'=>'Alta', 'categoria'=>'Estudos', 'usuario_id'=>'38'],
-
-            ['titulo'=> 'Aula de violão', 'descricao'=>'Estudar os acordes de A, B e F', 'dataInicio'=>'2026-03-23', 'dataTermino'=>'2026-03-30', 'status'=>'Pendente', 'prioridade'=>'Alta', 'categoria'=>'Estudos', 'usuario_id'=>'39'],
-
-            ['titulo'=> 'Lavar Louça', 'descricao'=>'lavar pratos, copos e garrafas', 'dataInicio'=>'2026-03-23', 'dataTermino'=>'2026-03-30', 'status'=>'Pendente', 'prioridade'=>'Alta', 'categoria'=>'Cuidado', 'usuario_id'=>'40'],
-
-            ['titulo'=> 'Relatório mensal', 'descricao'=>'Preparar o relatório de maio', 'dataInicio'=>'2026-03-23', 'dataTermino'=>'2026-03-30', 'status'=>'Pendente', 'prioridade'=>'Alta', 'categoria'=>'Trabalho', 'usuario_id'=>'41'],
-
-            ['titulo'=> 'Check-up equipe', 'descricao'=>'Reunião de alinhamento semanal', 'dataInicio'=>'2026-03-23', 'dataTermino'=>'2026-03-30', 'status'=>'Pendente', 'prioridade'=>'Alta', 'categoria'=>'Trabalho', 'usuario_id'=>'42'],
-
-            ['titulo'=> 'Compor', 'descricao'=>'Compor músicas para o próximo álbum', 'dataInicio'=>'2026-09-24', 'dataTermino'=>'2026-10-24', 'status'=>'Pendente', 'prioridade'=>'Alta', 'categoria'=>'Música', 'usuario_id'=>'43'],
-
-            ['titulo'=> 'ENEM', 'descricao'=>'último dia de estudo para o ENEM', 'dataInicio'=>'2026-09-24', 'dataTermino'=>'2026-10-24', 'status'=>'Pendente', 'prioridade'=>'Alta', 'categoria'=>'Estudos', 'usuario_id'=>'44'],
-
-            ['titulo'=> 'Fotos', 'descricao'=>'Seção de fotos para o novo single', 'dataInicio'=>'2026-09-24', 'dataTermino'=>'2026-10-24', 'status'=>'Pendente', 'prioridade'=>'Alta', 'categoria'=>'Trabalho', 'usuario_id'=>'45'],
-
-            ['titulo'=> 'Gravar Vídeo', 'descricao'=>'Gravar vídeo para o intagram', 'dataInicio'=>'2026-09-24', 'dataTermino'=>'2026-09-25', 'status'=>'Pendente', 'prioridade'=>'Baixa', 'categoria'=>'Lazer', 'usuario_id'=>'45'],
-
-            ['titulo'=> 'Pintar', 'descricao'=>'Pintar a próxima Mona Lisa', 'dataInicio'=>'2026-09-24', 'dataTermino'=>'2026-10-24', 'status'=>'Pendente', 'prioridade'=>'Alta', 'categoria'=>'Lazer', 'usuario_id'=>'47'],
+        DB::table('usuario')->insert([
+            ['nome'=> 'Penelope', 'email'=>'penelope20@gmail.com', 'senha'=>Hash::make('Odisseu20')],
+            ['nome'=> 'Pepino Gentille da Silva', 'email'=>'leguminoso67@gmail.com', 'senha'=>Hash::make('12345678')],
+            ['nome'=> 'Anna Baptista', 'email'=>'aninhazl@gmail.com', 'senha'=>Hash::make('12345678')],
+            ['nome'=> 'Giovanna', 'email'=>'giovanna@gmail.com', 'senha'=>Hash::make('12345678')],
+            ['nome'=> 'Taylor Swift', 'email'=>'swift1989@gmail.com', 'senha'=>Hash::make('Sh4k3It0ff')],
+            ['nome'=> 'Olivia Isabel Rodrigo', 'email'=>'imjustasourgirl@gmail.com', 'senha'=>Hash::make('Driv3rsLic3ns3')],
+            ['nome'=> 'Harry Styles', 'email'=>'harryzinho@gmail.com', 'senha'=>Hash::make('StylesHarry')],
+            ['nome'=> 'Bruno Mars', 'email'=>'brunomoon@gmail.com', 'senha'=>Hash::make('DieWithASmile')],
+            ['nome'=> 'Albert Eistain', 'email'=>'apenasumcararelativo@gmail.com', 'senha'=>Hash::make('Interestelar')],
+            ['nome'=> 'Leonardo Da Vinci', 'email'=>'codigoda20@gmail.com', 'senha'=>Hash::make('vitruviano')],
             
         ]);
     }
